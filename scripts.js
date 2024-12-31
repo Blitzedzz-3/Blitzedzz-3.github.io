721c70693f03d27a5ca11ff1fe90670354b39afc
Blitzedzz <blitzedzzz@proton.me>
2024-12-31 17:50:29 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,11 +2,9 @@ document.getElementById('copyButton').addEventListener('click', function() {
     var scriptText = document.getElementById('robloxScript');
     scriptText.select();
     document.execCommand('copy');
-
     const copyMessage = document.getElementById('copyMessage');
     copyMessage.textContent = 'Script copied to clipboard!';
     copyMessage.style.opacity = 1;
-
     setTimeout(function() {
         copyMessage.style.opacity = 0;
     }, 2000);
