2120211b60206f439c3aa2d26c803f98284cd9ab
Blitzedzz <blitzedzzz@proton.me>
2025-01-15 13:03:46 -0500

    Update scripts.js

--- Premium/scripts.js
+++ Premium/scripts.js
@@ -31,7 +31,7 @@ document.getElementById('PayWithCoinbase').addEventListener('click', function ()
 });
 
 document.getElementById('PayWithRobux').addEventListener('click', function () {
-    window.open('https://www.roblox.com/games/116740566274645/Super-skidibi-tycoon');
+    alert('Proceeding to pay with Robux!');
 });
 
 document.getElementById('paymentOverlay').addEventListener('click', function (e) {
