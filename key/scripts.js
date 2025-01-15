4946af24ba0118d7910b0172791caacd9e5b9ed9
Blitzedzz <blitzedzzz@proton.me>
2025-01-15 11:32:02 -0500

    Create scripts.js

---
+++ key/scripts.js
@@ -0,0 +1,6 @@
+document.getElementById('BuyButton').addEventListener('click', function() {
+ window.open("https://www.roblox.com/games/116740566274645/Super-skidibi-tycoon"); 
+});
+document.getElementById('SkipButton').addEventListener('click', function() {
+ window.open("https://www.roblox.com/games/116740566274645/Super-skidibi-tycoon"); 
+});
