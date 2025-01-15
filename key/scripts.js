f995d611233fc827a5c540fdb27d98689d1844d8
Blitzedzz <blitzedzzz@proton.me>
2025-01-15 11:35:33 -0500

    Update scripts.js

--- key/scripts.js
+++ key/scripts.js
@@ -1,6 +1,11 @@
 document.getElementById('BuyButton').addEventListener('click', function() {
  window.open("https://www.roblox.com/games/116740566274645/Super-skidibi-tycoon"); 
 });
-document.getElementById('SkipButton').addEventListener('click', function() {
- window.open("https://www.roblox.com/games/116740566274645/Super-skidibi-tycoon"); 
-});
+document.getElementById('SkipButton').disabled = true;
+
+setTimeout(function() {
+    document.getElementById('SkipButton').disabled = false;
+    document.getElementById('SkipButton').addEventListener('click', function() {
+        window.location.replace("https://keyyy.blitzedzz.workers.dev/");
+    });
+}, 10000);
